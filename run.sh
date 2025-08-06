0931e01a99b30608e0d3f376acb09ae24a8f2e3d
kyuu93201 <nguyenducmanhhd2011@gmail.com>
2025-08-06 12:15:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Dung lượng RAM tối đa và tối thiểu (sửa nếu cần)
-XMX=980M
-XMS=123M
+XMX=512M
+XMS=512M
 
 # Chạy server Paper
 java -Xmx$XMX -Xms$XMS -jar paper-1.21.7-32.jar nogui
